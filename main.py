57b017b8fe9081e7cf7e59652c7b0a70afb5a0cd
PRATHVIRAJ SHETTY <105969062+Prathviraj-Shetty@users.noreply.github.com>
2023-12-03 17:27:25 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+    SoH=[]
+    # SoH list maintains the SoH value of each battery
+    healthy_battery_count=0    #Maintains the healthy_battery_count
+    exchange_battery_count=0    #Maintains the exchange_battery_count
+    failed_battery_count=0  #Maintains the failed_battery_count
+    for battery in present_capacities:
+        cur_battery_SoH=100*battery/120
+        SoH.append(cur_battery_SoH)
+        #Classifying the batteries based on calculated SoH values int healthy,exchange and failed batteries
+        if(cur_battery_SoH>80):
+           healthy_battery_count=healthy_battery_count+1
+        elif(cur_battery_SoH>62 and cur_battery_SoH<80):
+           exchange_battery_count=exchange_battery_count+1
+        else:
+           failed_battery_count=failed_battery_count+1
+
+    return {
+    "healthy": healthy_battery_count,
+    "exchange": exchange_battery_count,
+    "failed": failed_battery_count
+    }
 
 
 def test_bucketing_by_health():
